3b41e9ed7cfb2237812d61d113a49a79f543a2ef
dwisky <daniel-ddw@hotmail.com>
2022-01-27 18:49:23 -0300

    test containers

--- app/src/test/java/br/com/wiskyacademy/hotel/gateways/inputs/http/AcomodacaoControllerTest.java
+++ app/src/test/java/br/com/wiskyacademy/hotel/gateways/inputs/http/AcomodacaoControllerTest.java
@@ -1,7 +1,14 @@
 package br.com.wiskyacademy.hotel.gateways.inputs.http;
 
 import static br.com.six2six.fixturefactory.Fixture.from;
+import static br.com.wiskyacademy.hotel.gateways.outputs.mysql.entities.AcomodacaoEntity_.DESCRICAO;
+import static br.com.wiskyacademy.hotel.gateways.outputs.mysql.entities.AcomodacaoEntity_.NOME;
 import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_CAPACIDADE_ALTA;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_CAPACIDADE_BAIXA;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_OUTRO_NOME_E_DESCRICAO;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_PRECO_ALTO;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_PRECO_BAIXO;
 import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_SEM_ID;
 import static java.lang.String.format;
 import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ONE;
@@ -141,5 +148,104 @@ public class AcomodacaoControllerTest extends IntegrationTest {
     mockMVC
         .perform(get(format(URL_WITH_PARAM, INTEGER_ONE)).contentType(APPLICATION_JSON))
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void devePesquisarUmaAcomodacaoSemFiltros() throws Exception {
+    final Acomodacao acomodacao =
+        acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_SEM_ID.name()));
+
+    mockMVC
+        .perform(get(format(URL)).contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.elementos", hasSize(1)))
+        .andExpect(jsonPath("$.elementos[0].id").value(acomodacao.getId()))
+        .andExpect(jsonPath("$.elementos[0].nome").value(acomodacao.getNome()))
+        .andExpect(jsonPath("$.elementos[0].descricao").value(acomodacao.getDescricao()))
+        .andExpect(jsonPath("$.elementos[0].capacidade").value(acomodacao.getCapacidade()))
+        .andExpect(jsonPath("$.elementos[0].preco").value(acomodacao.getPreco()))
+        .andExpect(jsonPath("$.pagina").value(0))
+        .andExpect(jsonPath("$.tamanho").value(20))
+        .andExpect(jsonPath("$.totalPaginas").value(1))
+        .andExpect(jsonPath("$.totalElementos").value(1));
+  }
+
+  @Test
+  public void devePesquisarUmaAcomodacaoPorNomeEDescricao() throws Exception {
+
+    acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_OUTRO_NOME_E_DESCRICAO.name()));
+    final Acomodacao acomodacao =
+        acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_SEM_ID.name()));
+
+    mockMVC
+        .perform(get(format(URL))
+            .param(NOME, acomodacao.getNome())
+            .param(DESCRICAO, acomodacao.getDescricao())
+            .contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.elementos", hasSize(1)))
+        .andExpect(jsonPath("$.elementos[0].id").value(acomodacao.getId()))
+        .andExpect(jsonPath("$.elementos[0].nome").value(acomodacao.getNome()))
+        .andExpect(jsonPath("$.elementos[0].descricao").value(acomodacao.getDescricao()))
+        .andExpect(jsonPath("$.elementos[0].capacidade").value(acomodacao.getCapacidade()))
+        .andExpect(jsonPath("$.elementos[0].preco").value(acomodacao.getPreco()))
+        .andExpect(jsonPath("$.pagina").value(0))
+        .andExpect(jsonPath("$.tamanho").value(20))
+        .andExpect(jsonPath("$.totalPaginas").value(1))
+        .andExpect(jsonPath("$.totalElementos").value(1));
+  }
+
+  @Test
+  public void devePesquisarUmaAcomodacaoPorPreco() throws Exception {
+
+    acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_PRECO_BAIXO.name()));
+    final Acomodacao acomodacao =
+        acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_PRECO_ALTO.name()));
+
+    mockMVC
+        .perform(get(format(URL))
+            .param("precoMaiorQue", acomodacao.getPreco().toString())
+            .param("precoMenorQue", acomodacao.getPreco().toString())
+            .contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.elementos", hasSize(1)))
+        .andExpect(jsonPath("$.elementos[0].id").value(acomodacao.getId()))
+        .andExpect(jsonPath("$.elementos[0].nome").value(acomodacao.getNome()))
+        .andExpect(jsonPath("$.elementos[0].descricao").value(acomodacao.getDescricao()))
+        .andExpect(jsonPath("$.elementos[0].capacidade").value(acomodacao.getCapacidade()))
+        .andExpect(jsonPath("$.elementos[0].preco").value(acomodacao.getPreco()))
+        .andExpect(jsonPath("$.pagina").value(0))
+        .andExpect(jsonPath("$.tamanho").value(20))
+        .andExpect(jsonPath("$.totalPaginas").value(1))
+        .andExpect(jsonPath("$.totalElementos").value(1));
+  }
+
+  @Test
+  public void devePesquisarUmaAcomodacaoPorCapacidade() throws Exception {
+
+    acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_CAPACIDADE_BAIXA.name()));
+    final Acomodacao acomodacao =
+        acomodacaoDatabaseGateway.save(from(Acomodacao.class).gimme(VALIDO_CAPACIDADE_ALTA.name()));
+
+    mockMVC
+        .perform(get(format(URL))
+            .param("capacidadeMaiorQue", acomodacao.getCapacidade().toString())
+            .param("capacidadeMenorQue", acomodacao.getCapacidade().toString())
+            .contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.elementos", hasSize(1)))
+        .andExpect(jsonPath("$.elementos[0].id").value(acomodacao.getId()))
+        .andExpect(jsonPath("$.elementos[0].nome").value(acomodacao.getNome()))
+        .andExpect(jsonPath("$.elementos[0].descricao").value(acomodacao.getDescricao()))
+        .andExpect(jsonPath("$.elementos[0].capacidade").value(acomodacao.getCapacidade()))
+        .andExpect(jsonPath("$.elementos[0].preco").value(acomodacao.getPreco()))
+        .andExpect(jsonPath("$.pagina").value(0))
+        .andExpect(jsonPath("$.tamanho").value(20))
+        .andExpect(jsonPath("$.totalPaginas").value(1))
+        .andExpect(jsonPath("$.totalElementos").value(1));
   }
 }
---
+++ app/src/test/java/br/com/wiskyacademy/hotel/usecases/UpdateAcomodacaoTest.java
@@ -0,0 +1,64 @@
+package br.com.wiskyacademy.hotel.usecases;
+
+import static br.com.six2six.fixturefactory.Fixture.from;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO;
+import static br.com.wiskyacademy.hotel.templates.FixtureCoreTemplates.VALIDO_OUTRO_NOME_E_DESCRICAO;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import br.com.wiskyacademy.hotel.UnitTest;
+import br.com.wiskyacademy.hotel.domains.Acomodacao;
+import br.com.wiskyacademy.hotel.domains.exceptions.ResourceNotFoundException;
+import br.com.wiskyacademy.hotel.gateways.AcomodacaoDatabaseGateway;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class UpdateAcomodacaoTest extends UnitTest {
+
+  @InjectMocks
+  private UpdateAcomodacao updateAcomodacao;
+
+  @Mock
+  private AcomodacaoDatabaseGateway acomodacaoDatabaseGateway;
+
+  @Captor
+  private ArgumentCaptor<Acomodacao> acomodacaoArgumentCaptor;
+
+  @Test
+  void deveExecutar() {
+
+    final Acomodacao acomodacao = from(Acomodacao.class).gimme(VALIDO.name());
+    final Acomodacao acomodacaoComOutroNome =
+        from(Acomodacao.class).gimme(VALIDO_OUTRO_NOME_E_DESCRICAO.name());
+
+    when(acomodacaoDatabaseGateway.findById(acomodacao.getId()))
+        .thenReturn(of(acomodacao));
+
+    updateAcomodacao.execute(acomodacao.getId(), acomodacaoComOutroNome);
+
+    Mockito.verify(acomodacaoDatabaseGateway).save(acomodacaoArgumentCaptor.capture());
+
+    final Acomodacao acomodacaoCapturada = acomodacaoArgumentCaptor.getValue();
+    assertEquals(acomodacaoComOutroNome.getNome(), acomodacaoCapturada.getNome());
+    assertEquals(acomodacaoComOutroNome.getDescricao(), acomodacaoCapturada.getDescricao());
+    assertEquals(acomodacaoComOutroNome.getCapacidade(), acomodacaoCapturada.getCapacidade());
+    assertEquals(acomodacaoComOutroNome.getPreco(), acomodacaoCapturada.getPreco());
+  }
+
+  @Test
+  void deveLancarExceptionResourceNotFoundQuandoNaoExistirAcomodacao() {
+    final Acomodacao acomodacao = from(Acomodacao.class).gimme(VALIDO.name());
+
+    when(acomodacaoDatabaseGateway.findById(acomodacao.getId())).thenReturn(empty());
+
+    assertThrows(ResourceNotFoundException.class,
+        () -> updateAcomodacao.execute(acomodacao.getId(), acomodacao));
+  }
+}
